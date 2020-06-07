9c6fa8645d66faa1610a6adbb24e04d394bab572
Jean-CNY <66203570+Jean-CNY@users.noreply.github.com>
2020-06-07 20:44:40 +0100

    Update python_challenges.py

--- python_challenges.py
+++ python_challenges.py
@@ -29,3 +29,61 @@ def remainder(num1, num2):
 	return (num1 * 2) % (num2 / 2)
 
 #print(remainder(15,14))
+
+#Creation of a function to find the first three multiples
+def first_three_multiples(num):
+	print(num*1)
+	print (num*2)
+	print (num*3)
+	return (num*3) 
+
+#print(first_three_multiples(10))
+
+#Creation of a function to calculate the tip with two arguments "total" and "percentage"
+def tip(total, percentage):
+	return total * (percentage / 100)
+
+#print(tip(10,25))
+
+#Creation of a repetitive function
+def introduction(first_name, last_name):
+	print(last_name, ", ", first_name, last_name)
+
+#print(introduction("James", "Bond"))
+
+#Creation of a calculator
+def dog_years(name, age):
+	dog_age = age * 7
+	print(name, ", you are ", dog_age, " years old in dog years")
+
+#print(dog_years("Lola",27))
+
+#Create a four parameters functions for calculation
+def lots_of_maths(a, b, c, d):
+	print(a+b)
+	print(c-d)
+	print((a+b) * (c-d))
+	return ((a+b) * (c-d)) % a
+
+#print(lots_of_maths(1, 2, 3, 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
